caacc0c04790f2898d9c56e6302d7ad9d97f8cfd
theSega <75573096+theSega@users.noreply.github.com>
2021-11-08 12:54:33 +0100

    Add files via upload

---
+++ Hito2 Triggers.sql
@@ -0,0 +1,9 @@
+USE Hito2;
+
+# a) Define un trigger para que cuando un jugador haya matado al menos 3 dragones,
+#    su vida aumente en 5.
+
+
+
+# b) Define un trigger para impedir que un escuadron mate un drag´on que no haya
+#    sido desbloqueado por alguno de sus integrantes.
