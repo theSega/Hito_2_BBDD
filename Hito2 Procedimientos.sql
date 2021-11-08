a5e198614d6375d70ddbf317fe4a45f9c76ec936
theSega <75573096+theSega@users.noreply.github.com>
2021-11-08 12:36:52 +0100

    Add files via upload

--- Hito2 Procedimientos.sql
+++ Hito2 Procedimientos.sql
@@ -1,9 +1,17 @@
 USE Hito2;
 
+# a) Define un procedimiento con un cursor para sumar 10 a la vida de cada guerrero
+#    que haya derrotado al menos 50 goblins.
 
-CREATE PROCEDURE ultima_conexion ()
-BEGIN
-	SET @A = SYSDATETIME(),
-	DELETE FROM jugador
-	where DATEDIFF(jugador.UltimaConexion,CONVERT(date,@A)>=60;
-END
+
+
+# b) Añade un atributo ‘ultima conexion’ a la tabla de personajes. Luego, crea un
+#    procedimiento que elimine los jugadores que no se hayan conectado en los ultimos
+#    60 dias. Puedes usar la funcion DATEDIFF1 para este fin.
+	
+    CREATE PROCEDURE ultima_conexion ()
+	BEGIN
+		SET @A = SYSDATETIME(),
+		DELETE FROM jugador
+		where DATEDIFF(jugador.UltimaConexion,CONVERT(date,@A)>=60;
+	END
